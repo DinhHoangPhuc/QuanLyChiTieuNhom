648d25e8697cf600378941e8c10abaa6eb1545f6
Dinh Hoang Phuc <98966221+DinhHoangPhuc@users.noreply.github.com>
2024-06-10 13:08:16 +0700

    hoan thanh chuc nang dang nhap

--- app/src/main/java/com/quanlychitieunhom/Phuc/DangNhap.java
+++ app/src/main/java/com/quanlychitieunhom/Phuc/DangNhap.java
@@ -1,9 +1,15 @@
 package com.quanlychitieunhom.Phuc;
 
+
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +17,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.quanlychitieunhom.QuocAnh.MainActivity;
 import com.quanlychitieunhom.R;
+import com.quanlychitieunhom.Thai.DanhSachNhom;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DangNhap extends AppCompatActivity {
 
     TextView txtQuenMatKhau;
     TextView txtDangKy;
     Button btnDangNhap;
+    EditText edtTaiKhoan;
+    EditText edtMatKhau;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +52,77 @@ public class DangNhap extends AppCompatActivity {
         getControl();
 
         txtQuenMatKhau.setOnClickListener(v -> {
-
         });
 
         txtDangKy.setOnClickListener(v -> {
             Intent intent = new Intent(DangNhap.this, DangKy.class);
             startActivity(intent);
         });
+
+        btnDangNhap.setOnClickListener(v -> {
+            if(edtTaiKhoan.getText().toString().isEmpty() || edtMatKhau.getText().toString().isEmpty()){
+                Toast.makeText(DangNhap.this, "Vui lòng nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                return;
+            } else {
+                callLoginApi(edtTaiKhoan.getText().toString(), edtMatKhau.getText().toString());
+                Intent intent = new Intent(DangNhap.this, DanhSachNhom.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    void callLoginApi(String username, String password){
+        String url = "http://10.0.2.2:8080/api/auth/login";
+
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("username", username);
+            requestBody.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, requestBody, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String token = response.optString("token");
+                        String error = response.optString("error");
+                        if(!token.isEmpty()) {
+                            Toast.makeText(DangNhap.this, token, Toast.LENGTH_LONG).show();
+//                            Context context = getActivity()
+                            SharedPreferences sharedPreferences;
+                            SharedPreferences.Editor editor;
+                            sharedPreferences = getApplicationContext().getSharedPreferences("dataLogin", MODE_PRIVATE);
+                            editor = sharedPreferences.edit();
+                            editor.putString("token", token);
+                            editor.apply();
+                            editor.commit();
+//                                Toast.makeText(DangKy.this, response.getString("message"), Toast.LENGTH_LONG).show();
+                        } else if(!error.isEmpty()) {
+                            Toast.makeText(DangNhap.this, error, Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(DangNhap.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.e("Error", error.getMessage());
+                    }
+                });
+
+        // Add the request to the RequestQueue
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
     }
 
     void getControl(){
         txtQuenMatKhau = findViewById(R.id.tvForgotPassword);
         txtDangKy = findViewById(R.id.tvSignUp);
         btnDangNhap = findViewById(R.id.btnDangNhap);
+        edtTaiKhoan = findViewById(R.id.edtUsername);
+        edtMatKhau = findViewById(R.id.edtPassword);
     }
 }
