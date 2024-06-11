2ed2c4cfd8251c21ffa9ed6f322b97ccaa2e9731
jinyin11 <kimngan4869@gmail.com>
2024-06-11 16:28:10 +0700

    doi goi api bang retrofit sang volley

--- app/src/main/java/com/quanlychitieunhom/Ngan/ChiaDeu.java
+++ app/src/main/java/com/quanlychitieunhom/Ngan/ChiaDeu.java
@@ -1,89 +1,131 @@
 package com.quanlychitieunhom.Ngan;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.quanlychitieunhom.R;
 
-import java.io.IOException;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChiaDeu extends AppCompatActivity {
-    private ApiService apiService;
+
+    private static final String BASE_URL = "http://10.0.2.2:8080/api/quy/";
+    private static final String AUTH_HEADER = "Authorization";
+    private String jwtToken;
+    private int groupId;
+    private TextView tvChiaDeu, tvTienHienTai;
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chia_deu);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
-        // Khởi tạo Retrofit Service Interface
-        apiService = RetrofitClient.getClient().create(ApiService.class);
+        // Lấy token JWT và groupId từ SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("dataLogin", Context.MODE_PRIVATE);
+        jwtToken = sharedPreferences.getString("token", null);
+        groupId = sharedPreferences.getInt("groupId", -1);
 
-        // Gọi API khi activity được tạo
-        callApi();
-        callApi2();
+        // Ánh xạ TextView
+        tvChiaDeu = findViewById(R.id.tv_chiadeu);
+        tvTienHienTai = findViewById(R.id.tv_tienHientai);
+
+        // Gọi phương thức chia tiền và lấy số tiền hiện tại khi hoạt động được tạo
+        callApiChiaDeu();
+        callApiTienHienTai();
     }
 
-    private void callApi() {
-        Call<ResponseBody> call = apiService.soTienHT();
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if (response.isSuccessful()) {
-                    // Xử lý dữ liệu trả về từ API
-                    // Hiển thị dữ liệu lên TextView
-                    TextView tvTienHT = findViewById(R.id.tv_tienHientai);
-                    try {
-                        tvTienHT.setText(response.body().string());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+    private void callApiChiaDeu() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String urlChiaDeu = BASE_URL + "chia-deu/" + groupId;
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, urlChiaDeu, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String message = response.getString("message");
+                            tvChiaDeu.setText(message);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
-                } else {
-                    // Xử lý khi API trả về lỗi
-                    Toast.makeText(ChiaDeu.this, "Có lỗi xảy ra!", Toast.LENGTH_SHORT).show();
-                }
-            }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", error.toString());
+                        Toast.makeText(ChiaDeu.this, "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
             @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                // Xử lý khi gọi API thất bại
-                Toast.makeText(ChiaDeu.this, "Không thể kết nối đến máy chủ!", Toast.LENGTH_SHORT).show();
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put(AUTH_HEADER, "Bearer " + jwtToken);
+                return headers;
             }
-        });
+        };
+
+        requestQueue.add(jsonObjectRequest);
     }
-    private void callApi2() {
-        Call<ResponseBody> call = apiService.chiaTien();
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                if (response.isSuccessful()) {
-                    // Xử lý dữ liệu trả về từ API
-                    // Hiển thị dữ liệu lên TextView
-                    TextView resultTextView = findViewById(R.id.tv_chiadeu);
-                    try {
-                        resultTextView.setText(response.body().string());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
+
+    private void callApiTienHienTai() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String urlTienHienTai = BASE_URL + "so-tien-hien-tai/" + groupId;
+
+        StringRequest stringRequest = new StringRequest
+                (Request.Method.GET, urlTienHienTai, new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        tvTienHienTai.setText(response);
                     }
-                } else {
-                    // Xử lý khi API trả về lỗi
-                    Toast.makeText(ChiaDeu.this, "Có lỗi xảy ra!", Toast.LENGTH_SHORT).show();
-                }
-            }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Error", error.toString());
+                        Toast.makeText(ChiaDeu.this, "Error: " + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
             @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                // Xử lý khi gọi API thất bại
-                Toast.makeText(ChiaDeu.this, "Không thể kết nối đến máy chủ!", Toast.LENGTH_SHORT).show();
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> headers = new HashMap<>();
+                headers.put(AUTH_HEADER, "Bearer " + jwtToken);
+                return headers;
             }
-        });
+        };
+
+        requestQueue.add(stringRequest);
     }
 }
